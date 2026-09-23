3cc7bddaeecebaea2d8364cd2bf4d8657dab4e33
Itay Bondar <ibondar04@gmail.com>
2026-09-23 15:09:13 +0300

    Add basic task endpoints

--- app/main.py
+++ app/main.py
@@ -1,8 +1,28 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+
+class Task(BaseModel):
+    title: str
+    completed: bool = False
+
+
+tasks = []
 
 
 @app.get("/")
 def root():
     return {"message": "Hello World"}
+
+
+@app.get("/tasks")
+def get_tasks():
+    return {"tasks": tasks}
+
+
+@app.post("/tasks")
+def create_task(task: Task):
+    tasks.append(task)
+    return task
