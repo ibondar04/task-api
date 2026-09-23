317266c69e3f978476a4b1ffa3446d106aa06983
Itay Bondar <ibondar04@gmail.com>
2026-09-23 15:59:33 +0300

    Add get task by ID endpoint

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 
 app = FastAPI()
@@ -20,6 +20,15 @@ def root():
 @app.get("/tasks")
 def get_tasks():
     return {"tasks": tasks}
+
+
+@app.get("/tasks/{task_id}")
+def get_task(task_id: int):
+    for task in tasks:
+        if task["id"] == task_id:
+            return task
+
+    raise HTTPException(status_code=404, detail="Task not found")
 
 
 @app.post("/tasks")
